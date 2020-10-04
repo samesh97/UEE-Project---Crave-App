27c209b33c084295f0d5d5591f6d0115c19ce8f8
Acer <pamoshakishani@gmail.com>
2020-10-04 22:32:40 +0530

    Restuarant list display interface

---
+++ app/src/main/java/com/crave/food/delivery/fragments/RestuarantFragment.java
@@ -0,0 +1,16 @@
+package com.crave.food.delivery.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.crave.food.delivery.R;
+
+public class RestuarantFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restuarant);
+    }
+}
