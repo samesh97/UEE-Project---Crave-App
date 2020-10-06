c4f62067509ed069c4382d48087011c92d52670f
samesh97 <Alahakoon123@>
2020-10-06 20:39:32 +0530

    minor changes

--- app/src/main/java/com/crave/food/delivery/fragments/RegisterFragment.java
+++ app/src/main/java/com/crave/food/delivery/fragments/RegisterFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,11 +13,13 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.crave.food.delivery.R;
+import com.crave.food.delivery.activities.MainActivity;
 
-public class RegisterFragment extends Fragment
-{
+public class RegisterFragment extends Fragment implements View.OnClickListener {
     private Context context;
     private FragmentManager manager;
+
+    private ImageView navigation_drawer_icon;
 
     public RegisterFragment(Context context,FragmentManager manager)
     {
@@ -29,6 +32,35 @@ public class RegisterFragment extends Fragment
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.register_layout,null);
+        initViews(view);
+        setData();
         return view;
+    }
+
+    private void setData()
+    {
+        navigation_drawer_icon.setOnClickListener(this);
+    }
+
+    private void initViews(View view)
+    {
+        navigation_drawer_icon = view.findViewById(R.id.navigation_drawer_icon);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        if(view == navigation_drawer_icon)
+        {
+            showNavigationDialog();
+        }
+    }
+    private void showNavigationDialog()
+    {
+        if(context instanceof MainActivity)
+        {
+            MainActivity activity = (MainActivity) context;
+            activity.showNavigationDialog();
+        }
     }
 }
