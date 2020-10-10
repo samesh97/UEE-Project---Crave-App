d4b11eb9ef79b23333ec75e44a36784ebfb2d51f
samesh97 <Alahakoon123@>
2020-10-10 19:56:34 +0530

    Removed unwanted ViewHolder

--- app/src/main/java/com/crave/food/delivery/adapters/FavoriteAdapter.java
+++ app/src/main/java/com/crave/food/delivery/adapters/FavoriteAdapter.java
@@ -4,17 +4,18 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.crave.food.delivery.FavoriteHolder;
 import com.crave.food.delivery.R;
 import com.crave.food.delivery.models.Favorite;
 
 import java.util.ArrayList;
 
-public class FavoriteAdapter extends RecyclerView.Adapter<FavoriteHolder>{
+public class FavoriteAdapter extends RecyclerView.Adapter<FavoriteAdapter.MyViewHolder>{
 
     Context c;
     ArrayList<Favorite> models;
@@ -24,15 +25,16 @@ public class FavoriteAdapter extends RecyclerView.Adapter<FavoriteHolder>{
         this.models = models;
     }
 
+
     @NonNull
     @Override
-    public FavoriteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.favorite_row,null);
-        return new FavoriteHolder(view);
+        return new MyViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull FavoriteHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
 
         holder.favTitle.setText(models.get(position).getTitle());
         holder.favDis.setText(models.get(position).getDiscription());
@@ -42,5 +44,18 @@ public class FavoriteAdapter extends RecyclerView.Adapter<FavoriteHolder>{
     @Override
     public int getItemCount() {
         return models.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView favTitle,favDis;
+        ImageView favIcon;
+        public MyViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+            favTitle = itemView.findViewById(R.id.titleFav);
+            favDis = itemView.findViewById(R.id.disFave);
+            favIcon = itemView.findViewById(R.id.foodIcon);
+        }
     }
 }
