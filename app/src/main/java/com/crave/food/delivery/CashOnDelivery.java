08d5350b77a813c1e26cd54ca45706dcad308a99
AILI <ailichandrasekara@gmail.com>
2020-10-10 16:36:55 +0530

    Payment pages connected to Cart

--- app/src/main/java/com/crave/food/delivery/CashOnDelivery.java
+++ app/src/main/java/com/crave/food/delivery/CashOnDelivery.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CartMain extends AppCompatActivity {
+public class CashOnDelivery extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cart_main);
+        setContentView(R.layout.activity_cash_on_delivery);
     }
-
-
 }
