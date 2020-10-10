4505d4784979face2e4130225262c560db524748
chathura99124 <chathura749@gmail.com>
2020-10-10 18:52:59 +0530

    notification offer favorite

--- app/src/main/java/com/crave/food/delivery/fragments/OrderFragment.java
+++ app/src/main/java/com/crave/food/delivery/fragments/OrderFragment.java
@@ -5,29 +5,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.crave.food.delivery.R;
 
-public class FavoriteFragment extends Fragment
-{
+public class OrderFragment extends Fragment {
 
     private Context context;
     private FragmentManager manager;
-    private RecyclerView foodList;
-    private FrameLayout home_sub_frame_layout;
-    private ImageView navigation_drawer_icon;
-    private EditText editText;
 
-    public FavoriteFragment(Context context, FragmentManager manager)
+    public OrderFragment(Context context, FragmentManager manager)
     {
         this.context = context;
         this.manager = manager;
@@ -37,7 +28,7 @@ public class FavoriteFragment extends Fragment
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.activity_favorite_main,container,false);
+        View view = inflater.inflate(R.layout.activity_cart_main,container,false);
         return view;
     }
 }
